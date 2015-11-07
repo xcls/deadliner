28e53489ba5ce9dd7916abc8e0784a1122d71097
Thomas Nys <nysthee@gmail.com>
2015-11-07 23:03:45 +0100

    Adds edit and destroy to dashboards_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :projects, only: [:index, :show]
   resources :deadlines, only: [:show]
-  resources :dashboards, only: [:new, :create]
+  resources :dashboards, only: [:new, :create, :update, :destroy]
 
-  get '/dashboards/:link_slug', to: 'dashboards#show', as: :dashboard
+  get '/dashboards/edit', to: 'dashboards#edit', as: :edit_dashboard
+  get '/dashboards/:link_slug', to: 'dashboards#show', as: :show_dashboard
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root 'home#index'
