1421e79d57d032ff0cb4c089123f0f31d89271bb
Maarten Claes <maartencls@gmail.com>
2015-11-07 15:16:57 +0100

    Wrapper around github pagination, so it works with Kaminari

--- app/models/project_manager.rb
+++ app/models/project_manager.rb
@@ -6,7 +6,7 @@ class ProjectManager
   end
 
   def projects(page: 1)
-    wrap_with Project, client.repos(nil, per_page: 100, page: page)
+    wrap_with Project, client.repos(nil, page: page)
   end
 
   def find_project(id)
@@ -36,7 +36,9 @@ class ProjectManager
   end
 
   def wrap_with(klass, xs)
-    xs.map { |m| klass.new(m) }
+    xs.map! { |m| klass.new(m) }
+    pagination = GithubPagination.new(client.last_response)
+    PaginatedArray.new(xs, pagination)
   end
 
   class Project
@@ -69,6 +71,71 @@ class ProjectManager
     def name
       original.title
     end
+  end
 
+  # Designed to work with Kaminari's `paginate`
+  class PaginatedArray
+    include Charlatan.new(:array)
+    attr_reader :pagination
+
+    def initialize(array, pagination)
+      super(array)
+      @pagination = pagination
+    end
+
+    delegate(
+      :current_page,
+      :limit_value,
+      :total_pages,
+      to: :pagination
+    )
+  end
+
+  # Designed to work with Kaminari's `paginate`
+  #
+  # @see PaginatedArray
+  # @see https://developer.github.com/guides/traversing-with-pagination/
+  class GithubPagination
+    include Charlatan.new(:response)
+
+    def next_page
+      @next_page ||= extract_page_for(:next)
+    end
+
+    def prev_page
+      @prev_page ||= extract_page_for(:prev)
+    end
+
+    def current_page
+      @current_page ||=
+        if next_page
+          next_page.to_i - 1
+        else
+          prev_page.to_i + 1
+        end
+    end
+
+    def limit_value
+      href_for(:next).match(/\bper_page=(\d+)/).try(:[], 1) || 30
+    end
+    alias_method :per_page, :limit_value
+
+    def total_pages
+      (extract_page_for(:last) || current_page).to_i
+    end
+
+    private
+
+    def extract_page_for(attr)
+      extract_page(href_for(attr))
+    end
+
+    def href_for(attr)
+      response.rels[attr] ? response.rels[attr].href : ""
+    end
+
+    def extract_page(href)
+      href.match(/\bpage=(\d+)/).try(:[], 1)
+    end
   end
 end
