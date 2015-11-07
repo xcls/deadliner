a050abb627ee634e0bfeb7a17499d52d849fedbf
Maarten Claes <maartencls@gmail.com>
2015-11-07 15:52:05 +0100

    Remove ap

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    Rails.logger.ap request.env["omniauth.auth"]
     user = User.from_omniauth(request.env["omniauth.auth"])
 
     if user.persisted?
