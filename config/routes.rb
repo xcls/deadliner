bad2886a7052f866354606acf8c5f1e8906a4c11
Quinten Claes <quintencls@gmail.com>
2015-11-07 11:29:28 +0100

    Add sign out route and link

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root 'home#index'
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 end
