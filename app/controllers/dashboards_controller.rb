49277dfd75b1d7dbd4fcba754b6b49ca8720b6b2
Quinten Claes <quintencls@gmail.com>
2015-11-07 20:30:59 +0100

    Add show actions for the dashboards, wip

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,22 +2,42 @@ class DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @dashboard = Dashboard.new
+    @dashboard = current_user.dashboards.new()
+    @project_identifier = params[:project_identifier]
   end
 
   def create
-    @dashboard = current_user.dashboard.create(dashboard_params)
+    @dashboard = current_user.dashboards.create(dashboard_params)
 
     if @dashboard.save
-      render :show
+      redirect_to dashboard_path(link_slug: @dashboard.link_slug)
     else
       render :new
     end
   end
 
+  def show
+    dashboard = Dashboard.find_by_link_slug(params[:link_slug])
+    pm = ProjectManager.new(dashboard.user)
+    render locals: {
+      project: pm.find_project(dashboard.project_identifier),
+      deadlines: pm.deadlines_for(dashboard.project_identifier),
+    }
+  end
+
+  def show_milestone
+    dashboard = Dashboard.find_by_link_slug(params[:link_slug])
+    pm = ProjectManager.new(dashboard.user)
+    render locals: {
+      deadline: pm.find_deadline(params[:project_id], params[:id]),
+      tasks: pm.tasks_for(params[:project_id], params[:id])
+    }
+  end
+
   private
 
   def dashboard_params
-    params.require(:dashboard).permit(:password, :show_tasks, :published)
+    params.require(:dashboard).permit(:password, :show_tasks, :published,
+                                      :project_identifier)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :projects, only: [:index, :show]
   resources :deadlines, only: [:show]
-  resources :dashboards
+  resources :dashboards, only: [:new, :create]
+
+  get '/dashboards/:link_slug', to: 'dashboards#show', as: :dashboard
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root 'home#index'
