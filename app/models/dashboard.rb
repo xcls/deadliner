23d19a0660516ba0b63cb8562ebaf17ff0454c47
Maarten Claes <maartencls@gmail.com>
2015-11-08 15:45:06 +0100

    Validate presence of slug

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -15,10 +15,12 @@
 
 class Dashboard < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :user_id, :project_uid
+
+  validates_format_of :slug, with: /\A[a-zA-Z0-9\-_]+\z/
+  validates_presence_of :user_id, :project_uid, :slug
   validates_uniqueness_of :project_uid, scope: :user_id
   validates_uniqueness_of :slug
-  validates_format_of :slug, with: /\A[a-zA-Z0-9\-_]+\z/
+
   before_validation :generate_slug, on: :create
 
   protected
